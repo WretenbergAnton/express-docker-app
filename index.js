5ab659039f7e2dce811c2fd9243b0e7f7067c543
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2026-03-02 00:08:44 +0100

    test: add version to response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3040
 const APP_NAME = process.env.APP_NAME || 'Express App'
 
 app.get('/', (req, res) => {
-  res.send({ message:`Hej från ${APP_NAME}!` })
+  res.json({ message: `Hej från ${APP_NAME}!`, version: '2.0' });
 })
 
 app.listen(PORT, () => {
