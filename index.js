225a12c9e0ab64a637e2450deea578d42f73ada0
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2026-03-02 00:28:14 +0100

    feat: add mongoose and connect to mongodb atlas

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+import 'dotenv/config'
 import express from 'express'
+
+import connectDB from './src/config/db.js'
 
 const app = express()
 const PORT = process.env.PORT || 3040
 const APP_NAME = process.env.APP_NAME || 'Express App'
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ message: `Hej från ${APP_NAME}!`, version: '2.0' });
@@ -10,4 +15,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB()
 })
